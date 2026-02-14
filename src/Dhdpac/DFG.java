ce07cf2da16ed57fbab74415328060c537c7834e
Galois5212 <127105425+Galois5212@users.noreply.github.com>
2026-02-14 19:45:14 +0900

    first commit

---
+++ src/Dhdpac/DFG.java
@@ -0,0 +1,13 @@
+package Dhdpac;
+
+public class DFG {
+ int a=0;
+
+ publec void DFG(){
+    if (a>10){
+        System.out.println("10以上");
+ }
+    else{
+        System.out.println("10以下");
+}
+}}
---
+++ src/Dhdpac/Fgr.java
@@ -0,0 +1,13 @@
+package Dhdpac;
+import java.io.Console;
+
+public class Fgr {
+    public static void main(String[] args) {
+        Console console = System.console();
+        String input = console.readLine("数字を入力してください: ");
+        int a = Integer.parseInt(input);
+        pubric DFG(){
+            
+        }
+    }
+}
